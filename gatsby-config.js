9b60ffb232504e51c0c8cca8b2846663d0c72645
ubaidmalik0303 <ubaidmalik0303@gmail.com>
2022-04-23 18:33:49 +0500

    muffshell

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,11 +11,24 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MuffShell`,
+        short_name: `MuffShell`,
+        icon: "src/images/favicon.ico",
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `white`,
+        display: `standalone`,
       },
     },
   ],
